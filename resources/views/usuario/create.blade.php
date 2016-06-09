fea7c9e486cd98e7db46201d6f729c50d1cc038e
juanes <juanes>
2016-06-09 19:41:43 -0400

    commit 09/06

--- resources/views/usuario/create.blade.php
+++ resources/views/usuario/create.blade.php
@@ -1,42 +1,10 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-	<meta charset="UTF-8">
-	<title>Formulario-Login</title>
-	<meta name="viewport" content="with=device-with, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-
-	<style>
-      body{
-        padding-top: 40px;
-        padding-bottom: 40px;
-      }
-      .logueo{
-         max-width: 330px;
-         padding: 15px;
-         margin: 0 auto;
-      }
-      #sha{
-      	max-width: 340px;
-      	-webkit -box-shadow: 0px 0px 18px 0px rgba(48,50,50,0,48);
-      	-moz -box-shadow: 0px 0px 18px 0px rgba(48,50,50,0,48);
-      	box-shadow: 0px 0px 18px 0px rgba(48,50,50,0,48);
-      	border-radius:6%;
-      }
-      #login{
-      	width: 96px;
-      	height: 96px;
-      	margin: 0px auto 10px;
-      	display: block;
-      	border-radius: 50%;
-      }
-	</style>
-</head>
-<body>
-	<div class="container well" id="sha">
+@extends('layouts.principal')
+@section('content')
+<div class="container well" id="sha">
 		<div class="row">
 			<div class="col-xs-12">
-				<img src="img/login.png" class="img-responsive" id="login">
+				<img src="img/login.png" class="img-
+                responsive" id="login">
 			</div>
 		</div>
 		<form class="logueo" action="check.php" method="POST">
@@ -62,9 +30,29 @@
 
 
 	</div>
-
-	<script src="js/jquery-1.11.1.min.js"></script>
-	<script src="js/bootstrap.js"></script>
-
-</body>
-</html>
+@stop
+	<style>
+      body{
+        padding-top: 40px;
+        padding-bottom: 40px;
+      }
+      .logueo{
+         max-width: 330px;
+         padding: 15px;
+         margin: 0 auto;
+      }
+      #sha{
+      	max-width: 340px;
+      	-webkit -box-shadow: 0px 0px 18px 0px rgba(48,50,50,0,48);
+      	-moz -box-shadow: 0px 0px 18px 0px rgba(48,50,50,0,48);
+      	box-shadow: 0px 0px 18px 0px rgba(48,50,50,0,48);
+      	border-radius:6%;
+      }
+      #login{
+      	width: 96px;
+      	height: 96px;
+      	margin: 0px auto 10px;
+      	display: block;
+      	border-radius: 50%;
+      }
+	</style>
